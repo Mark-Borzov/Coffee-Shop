d7ea7fa185140cf03da472dc3d5ca7c3f289caec
Mark Borzov <stevelincoln@bk.ru>
2023-04-06 15:04:09 +0300

    The "explore" And "about" Sections Are Completed

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,3 +16,15 @@ $(document).ready(function() {
         $('body').toggleClass('lock'); 
     });
 });
+
+const anchors = document.querySelectorAll('a[href^="#"]')
+for(let anchor of anchors) {
+  anchor.addEventListener("click", function(e) {
+    e.preventDefault()
+    const goto = anchor.hasAttribute('href') ? anchor.getAttribute('href') : 'body'
+    document.querySelector(goto).scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    })
+  })
+}
